577b8a75353ca349bd3dc4a3964a5caf9e4acc92
Jason-Guo1999 <106792458+Jason-Guo1999@users.noreply.github.com>
2023-09-26 21:21:58 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,12 +3,11 @@ Test goes here
 
 """
 
-import polars as pl
-
-def test_data():
-    data = pl.read_csv("data.csv")
-    assert data.height > 0
+import sqltie3
 
 if __name__ == "__main__":
-    test_data()
-    print("CI passed.")
+    conn = sqlite3.connect('mydatabase.db')
+    cursor = conn.cursor()
+    cursor.execute('count * FROM users')
+    num = cursor.fetchall()
+    assert(num==2)
