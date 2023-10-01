d01d347f4a03b33ca0a2832448e65d68257ddbcb
Jason-Guo1999 <106792458+Jason-Guo1999@users.noreply.github.com>
2023-10-01 02:37:50 +0000

    final commit

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import sqlite3
 
 if __name__ == "__main__":
-    conn = sqlite3.connect('mydatabase.db')
+    conn = sqlite3.connect("mydatabase.db")
     cursor = conn.cursor()
     # Create a table
-    cursor.execute('''
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY,
-            username TEXT NOT NULL,
+            username TEXT NOT NULL
         )
-    ''')
+    """)
+
 
     # Update
-    cursor.execute('INSERT INTO users (username) VALUES ('zg105'))
-    cursor.execute('INSERT INTO users (username) VALUES ('yl794'))
+    cursor.execute("INSERT INTO users (username) VALUES ('zg105')")
+    cursor.execute("INSERT INTO users (username) VALUES ('yl794')")
     
     conn.commit()
 
-    # Read
-    cursor.execute('SELECT username FROM users')
-
     # Delete
-    cursor.execute('DELETE from users where username = zg105')
+    cursor.execute("DELETE from users where username = 'zg105'")
+
+    # Read
+    cursor.execute("SELECT username FROM users")
     users = cursor.fetchall()
     
     for user in users:
